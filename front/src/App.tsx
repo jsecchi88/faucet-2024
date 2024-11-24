da4bfa539ac069330ab32857171bcf3b78159acc
jsecchi88 <91900285+jsecchi88@users.noreply.github.com>
2024-11-24 15:58:47 -0300

    v.0.1

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,54 +1,33 @@
-import { useEffect, useState } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-/*
-curl --location 'localhost:5556/' \
---header 'Content-Type: application/json' \
---data '{
-	"jsonrpc":"2.0",
-	"method":"eth_getBalance",
-	"params":[
-		"0x7256ad738AA6A91a06801DCfDd1DE8DE20b1F91D", 
-		"latest"
-	],
-	"id":1
-}'
-*/
-// {"jsonrpc":"2.0","id":1,"result":"0x8ac7230489e80000"}
+import { Dashboard } from "./components/Dashboard";
+import { Home } from "./components/Home";
+import { Balance } from "./components/Balance";
+import { Faucet } from "./components/Faucet";
+import { Transfer } from "./components/Transfer";
+import { createContext, useState } from "react";
 
-type Data = {
-  jsonrpc: string
-  id: number
-  result: string
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "faucet", element: <Faucet /> },
+      { path: "balance", element: <Balance /> },
+      { path: "transfer", element: <Transfer /> },
+    ],
+  },
+]);
 
-export default function Home() {
-  const [data, setData] = useState<Data | null>(null)
-  useEffect(() => {
-    fetch('http://localhost:5556/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ 
-        jsonrpc: "2.0",
-        method: "eth_getBalance",
-        params: [
-          "0x7256ad738AA6A91a06801DCfDd1DE8DE20b1F91D",
-          "latest"
-        ],
-        id: 1
-      })
-    })
-      .then((res: Response) => res.json())
-      .then(setData)
-      .catch(console.error);
-  }, []);
-  
-  if (!data) return <div>Loading...</div>;
-  
-  return (
-    <div>
-      {Number(data.result) / 10**18} ETH
-    </div>
-  );
+export const UserContext = createContext({});
+
+export default function App() {
+  const [state, setState] = useState({
+    acc: "",
+  });
+
+  return  <UserContext.Provider value={{ state, setState }}>
+               <RouterProvider router={router} />
+           </UserContext.Provider>
 }
